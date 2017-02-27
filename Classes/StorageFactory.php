abc9c37b495da837e45c7708f48c29144c44f137
Robert Lemke <robert@flownative.com>
2017-02-27 20:19:54 +0100

    FEATURE: Provide credentials private key via ENV
    This feature allows for specifying the P12 private key as a
    base64-encoded string alternatively to a path and filename.
    That allows for file-less scenarios with environment variables
    only.
    
    See README.md for details.

--- Classes/StorageFactory.php
+++ Classes/StorageFactory.php
@@ -46,17 +46,21 @@ class StorageFactory
             throw new Exception(sprintf('The specified Google Cloud Storage credentials profile "%s" does not exist, please check your settings.', $credentialsProfileName), 1446553024);
         }
 
-        if (substr($this->credentialProfiles[$credentialsProfileName]['credentials']['privateKeyP12PathAndFilename'], 0, 1) !== '/') {
-            $privateKeyPathAndFilename = FLOW_PATH_ROOT . $this->credentialProfiles[$credentialsProfileName]['credentials']['privateKeyP12PathAndFilename'];
+        if (!empty($this->credentialProfiles[$credentialsProfileName]['credentials']['privateKeyP12Base64Encoded'])) {
+            $privateKey = base64_decode($this->credentialProfiles[$credentialsProfileName]['credentials']['privateKeyP12Base64Encoded']);
         } else {
-            $privateKeyPathAndFilename = $this->credentialProfiles[$credentialsProfileName]['credentials']['privateKeyP12PathAndFilename'];
+            if (substr($this->credentialProfiles[$credentialsProfileName]['credentials']['privateKeyP12PathAndFilename'], 0, 1) !== '/') {
+                $privateKeyPathAndFilename = FLOW_PATH_ROOT . $this->credentialProfiles[$credentialsProfileName]['credentials']['privateKeyP12PathAndFilename'];
+            } else {
+                $privateKeyPathAndFilename = $this->credentialProfiles[$credentialsProfileName]['credentials']['privateKeyP12PathAndFilename'];
+            }
+
+            if (!file_exists($privateKeyPathAndFilename)) {
+                throw new Exception(sprintf('The Google Cloud Storage private key file "%s" does not exist. Either the file is missing or you need to adjust your settings.', $privateKeyPathAndFilename), 1446553054);
+            }
+            $privateKey = file_get_contents($privateKeyPathAndFilename);
         }
 
-        if (!file_exists($privateKeyPathAndFilename)) {
-            throw new Exception(sprintf('The Google Cloud Storage private key file "%s" does not exist. Either the file is missing or you need to adjust your settings.', $privateKeyPathAndFilename), 1446553054);
-        }
-
-        $privateKey = file_get_contents($privateKeyPathAndFilename);
         $credentials = new \Google_Auth_AssertionCredentials(
             $this->credentialProfiles[$credentialsProfileName]['credentials']['clientEmail'],
             [ \Google_Service_Storage::DEVSTORAGE_READ_WRITE ],
@@ -64,7 +68,7 @@ class StorageFactory
         );
 
         $temporaryTargetPathAndFilename = $this->environment->getPathToTemporaryDirectory() . 'Flownative_Google_CloudStorage_Temp';
-       
+
         $googleClient = new \Google_Client();
         $googleClient->setClassConfig('Google_Cache_File', 'directory',$temporaryTargetPathAndFilename);
         $googleClient->setAssertionCredentials($credentials);
